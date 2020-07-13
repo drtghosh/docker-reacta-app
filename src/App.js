d3ccd55c4cae311c112924c6bb3c10c50ef1f4a3
Debabrata Ghosh <32821041+drtghosh@users.noreply.github.com>
2020-07-13 19:55:28 +0530

    docker files added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Did that by rebuilding..... Now doing without rebuilding! Changed again :(
         </p>
         <a
           className="App-link"
